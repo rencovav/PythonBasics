fa1453797cd2edc10bc91e138668046b54ce2bef
Vlasta Rencova <rencovav@seznam.cz>
2022-01-08 17:38:46 +0100

    Unittesting

--- Python-Basics/test_sum.py
+++ Python-Basics/test_sum.py
@@ -4,8 +4,8 @@ import testing as t
 
 class MyTestCase(unittest.TestCase):
     def test_sum(self):
-        self.assertEqual(t.positive_sum([1, 2, 3]), 6)
-        self.assertRaises(ValueError, t.positive_sum, [-1, 3, 6])
+        self.assertEqual(t.positive_sum([1, 2, 3]), 6)  # test if sum works
+        self.assertRaises(ValueError, t.positive_sum, [-1, 3, 6])  # test if exception raises
         self.assertRaises(TypeError, t.positive_sum, 3)
 
 
